845f9724f5e0dbeca7534bfb71de84bfddd3ce51
ShenitaRose <simsationelle@gmail.com>
2021-10-28 14:53:52 +0100

    added the search engine

--- src/app.js
+++ src/app.js
@@ -36,10 +36,21 @@ iconElement.setAttribute(
     "alt", response.data.weather[0].description);
 }
 
-
+function search(city) {
 let apiKey = "9cb245c8974a9aa2bee9c6e33954b52a";
-let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
+}
 
 
-axios.get(apiUrl).then(displayTemperature) ;
+function handleSubmit(event) {
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+search("New York");
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
